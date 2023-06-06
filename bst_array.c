5833e272029fbeba6e52917a4659dd04a6d8d503
wcs <iam.william.schmidt@gmail.com>
2023-06-06 00:23:32 -0700

    fucntions for arbitrary array types

--- bst_array.c
+++ bst_array.c
@@ -2,33 +2,32 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define ARR_TYPE uint32_t
 
-typedef struct bst_node_t
-{
-    ARR_TYPE val;
-    bst_node_t *l;
-    bst_node_t *r;
-} bst_node_t;
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-void merge(ARR_TYPE *arr, size_t l, size_t m, size_t r, int32_t (*cmp)(ARR_TYPE, ARR_TYPE))
+void merge(ARR_TYPE *arr, size_t l, size_t m, size_t r)
 {
     size_t i, j, k;
-    size_t Ln = m - l + 1;
-    size_t Rn = r - m;
+    size_t n1 = m - l + 1;
+    size_t n2 = r - m;
 
-    ARR_TYPE L[Ln], R[Rn];
-    memcpy(L, &arr[l], sizeof(L[0]) * Ln);
-    memcpy(R, &arr[m + 1], sizeof(R[0]) * Rn);
+    ARR_TYPE L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
 
     i = 0;
     j = 0;
-    k = l;
 
-    while (i < Ln && j < Rn)
+    k = l;
+    while (i < n1 && j < n2)
     {
-        if (cmp(L[i], R[j]) >= 0)
+        if (L[i] <= R[j])
         {
             arr[k] = L[i];
             i++;
@@ -41,14 +40,14 @@ void merge(ARR_TYPE *arr, size_t l, size_t m, size_t r, int32_t (*cmp)(ARR_TYPE,
         k++;
     }
 
-    while (i < Ln)
+    while (i < n1)
     {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    while (j < Rn)
+    while (j < n2)
     {
         arr[k] = R[j];
         j++;
@@ -56,28 +55,28 @@ void merge(ARR_TYPE *arr, size_t l, size_t m, size_t r, int32_t (*cmp)(ARR_TYPE,
     }
 }
 
-void merge_sort(ARR_TYPE *arr, size_t l, size_t r, int32_t (*cmp)(ARR_TYPE, ARR_TYPE))
+void merge_sort(ARR_TYPE *arr, size_t l, size_t r)
 {
     if (l < r)
     {
         size_t m = l + (r - l) / 2;
 
-        merge_sort(arr, l, m, cmp);
-        merge_sort(arr, m + 1, r, cmp);
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
 
-        merge(arr, l, m, r, cmp);
+        merge(arr, l, m, r);
     }
 }
 
-void array_sort(ARR_TYPE *arr, size_t n, int32_t (*cmp)(ARR_TYPE, ARR_TYPE))
+void array_sort(ARR_TYPE *arr, size_t n)
 {
-    merge_sort(arr, 0, n - 1, cmp);
+    merge_sort(arr, 0, n - 1);
 }
 
 void array_reverse(ARR_TYPE *arr, size_t n)
 {
     ARR_TYPE *front = arr;
-    ARR_TYPE *back = arr[n - 1];
+    ARR_TYPE *back = &arr[n - 1];
     while (front < back)
     {
         ARR_TYPE tmp = *front;
@@ -88,69 +87,171 @@ void array_reverse(ARR_TYPE *arr, size_t n)
     }
 }
 
-void array_rotate(ARR_TYPE *arr, size_t n, int32_t d)
+void reverse(void *arr, const size_t elem_size, const size_t elem_cnt)
 {
-    d = d % n;
-    d = (d < 0) ? d + n : d;
-    array_reverse(arr, d);
-    array_reverse(arr + d, n - d);
-    array_reverse(arr, n);
+    uint8_t *front = arr;
+    uint8_t *back = arr + (elem_cnt - 1) * elem_size;
+
+    while (front < back)
+    {
+        uint8_t *next_front = front + elem_size;
+        while (front < next_front)
+        {
+            uint8_t tmp = *front;
+            *front = *back;
+            *back = tmp;
+
+            front++;
+            back++;
+        }
+
+        back -= elem_size * 2;
+    }
 }
 
-size_t inverted_faro_shuffle(ARR_TYPE *arr, size_t first, size_t last)
+void rotate(void *arr, const size_t elem_size, const size_t elem_cnt, int32_t d)
 {
-    size_t n = last - first;
+    d = d % elem_cnt;
+    d = (d < 0) ? d + elem_cnt : d;
 
-    if (n == 1)
+    reverse(arr, elem_size, d);
+    reverse(arr + d * elem_size, elem_size, elem_cnt - d);
+    reverse(arr, elem_size, elem_cnt);
+}
+
+// Perform faro shuffle inverse on an array. O(n) time, O(n) space
+void array_invert_faro_shuffle(ARR_TYPE *arr, size_t n)
+{
+    ARR_TYPE tmp[n];
+
+    size_t m = ((n + 1) / 4) * 2;
+    size_t i = 0;
+
+    // move even indices to front half
+    size_t k = 0;
+    for (; k < n; ++i)
     {
-        return last;
+        tmp[i] = arr[k];
+        k += 2;
     }
 
-    size_t m = first + (((n + 1) >> 4) << 1);
-}
-
-size_t next_pow_2(size_t n)
-{
-    n--;
-    n |= n >> 1;
-    n |= n >> 2;
-    n |= n >> 4;
-    n |= n >> 8;
-    n |= n >> 16;
-    n++;
-
-    return n;
-}
-
-size_t max_leaves(size_t n)
-{
-    n = next_pow_2(n) - 1;
-
-    return n / 2;
-}
-
-size_t height(size_t n)
-{
-    size_t h = 0;
-    while (n)
+    // move odd indices to back half
+    k = 1;
+    for (; k < n; ++i)
     {
-        n >>= 1;
+        tmp[i] = arr[k];
+        k += 2;
+    }
+
+    // copy result back to arr
+    for (i = 0; i < n; ++i)
+    {
+        arr[i] = tmp[i];
+    }
+}
+
+void inverse_faro_shuffle(void *arr, const size_t elem_size, const size_t elem_cnt)
+{
+    size_t arr_size = elem_size * elem_cnt;
+    uint8_t tmp[arr_size];
+
+    size_t i = 0;
+
+    size_t j = 0;
+    while (j < arr_size)
+    {
+        size_t k = i + elem_size;
+        while (i < k)
+        {
+            tmp[i] = ((uint8_t *)arr)[j];
+            ++i;
+            ++j;
+        }
+        j += elem_size;
+    }
+
+    j = elem_size;
+    while (j < arr_size)
+    {
+        size_t k = i + elem_size;
+        while (i < k)
+        {
+            tmp[i] = ((uint8_t *)arr)[j];
+            ++i;
+            ++j;
+        }
+        j += elem_size;
+    }
+
+    // copy result back to arr
+    for (i = 0; i < arr_size; ++i)
+    {
+        ((uint8_t *)arr)[i] = tmp[i];
+    }
+}
+
+void array_to_complete_btree(void *arr, const size_t elem_size, const size_t elem_cnt)
+{
+    uint8_t h = 1;
+    while ((2 << h) - 1 < elem_cnt)
+    {
         h++;
     }
+
+    for (uint8_t lvl = h; lvl > 0; lvl--)
+    {
+        size_t es = MIN((2 << lvl) - 1, elem_cnt);
+        size_t lc = MIN(1 << lvl, elem_cnt - ((1 << lvl) - 1));
+
+        inverse_faro_shuffle(arr, elem_size, 2 * lc - 1);
+        rotate(arr, elem_size, es, lc);
+    }
 }
 
-void array_sorted_to_complete_bst(ARR_TYPE *arr, ARR_TYPE *res, size_t level_leaves, size_t n)
+void array_sorted_to_complete_bst(ARR_TYPE *arr, size_t n)
 {
-    size_t p = 0;
-    size_t m = max_leaves(n);
-    if ((m / 2 - 1) <= (n - m))
+    uint8_t h = 1;
+    while ((2 << h) - 1 < n)
     {
-        p = m - 1;
+        h++;
     }
-    else
+
+    for (uint8_t lvl = h; lvl > 0; lvl--)
     {
-        p = n - m / 2;
+        size_t es = MIN((2 << lvl) - 1, n);
+        size_t lc = MIN(1 << lvl, n - ((1 << lvl) - 1));
+
+        inverse_faro_shuffle(arr, sizeof(ARR_TYPE), 2 * lc - 1);
+        rotate(arr, sizeof(ARR_TYPE), es, lc);
     }
+}
+
+void array_unsorted_to_complete_bst(ARR_TYPE *arr, size_t n)
+{
+    array_sort(arr, n);
+    array_sorted_to_complete_bst(arr, n);
+}
+
+bool array_bst_search(ARR_TYPE *arr, size_t n, ARR_TYPE key)
+{
+    size_t i = 0;
+    while (i < n)
+    {
+        if (key < arr[i])
+        {
+            i = 2 * i + 1;
+        }
+        else if (key > arr[i])
+        {
+            i = 2 * i + 2;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 void print_array(ARR_TYPE *arr, size_t n)
@@ -165,27 +266,26 @@ void print_array(ARR_TYPE *arr, size_t n)
 
 int main()
 {
-    ARR_TYPE a[] = {0, 1, 2, 3, 4, 5, 6, 7};
+    ARR_TYPE a[] = {0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11};
     size_t an = sizeof(a) / sizeof(a[0]);
 
     print_array(a, an);
     printf("\n");
 
-    ARR_TYPE a_bst[an];
-
-    array_sorted_to_bst(a, a_bst, 0, an, 0);
-    print_array(a_bst, an);
+    array_sorted_to_complete_bst(a, an);
+    print_array(a, an);
     printf("\n\n");
 
-    ARR_TYPE b[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    ARR_TYPE b[] = {10, 4, 11, 1, 0, 2, 5, 9, 3, 8, 7};
     size_t bn = sizeof(b) / sizeof(b[0]);
 
     print_array(b, bn);
     printf("\n");
 
-    ARR_TYPE b_bst[bn];
-
-    array_sorted_to_bst(b, b_bst, 0, bn, 0);
-    print_array(b_bst, bn);
+    array_unsorted_to_complete_bst(b, bn);
+    print_array(b, bn);
     printf("\n\n");
+
+    printf("%s\n", array_bst_search(b, bn, 2) ? "FOUND" : "NOT FOUND");
+    printf("%s\n", array_bst_search(b, bn, 6) ? "FOUND" : "NOT FOUND");
 }
