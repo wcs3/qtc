bb8e8eca94075ae7a1356bb34176154307c61779
William Schmidt <wcschmidt99@gmail.com>
2023-06-23 18:57:17 -0700

    cleaned up repo

--- rle.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RLE_H__
-#define __RLE_H__
-
-#include <stdlib.h>
-#include <stdint.h>
-
-
-
-#endif // __RLE_H__
---
+++ test_assets/assets.h
@@ -0,0 +1,40 @@
+#ifndef __ASSETS_H__
+#define __ASSETS_H__
+
+#define BOOK_BMP_WIDTH 2290
+#define BOOK_BMP_HEIGHT 1841
+extern const unsigned char book_bmp_bits[];
+
+#define BOOK_BMP_SM_WIDTH 512
+#define BOOK_BMP_SM_HEIGHT 412
+extern const unsigned char book_bmp_sm_bits[];
+
+#define GLOBE_WIDTH 1024
+#define GLOBE_HEIGHT 1024
+extern const unsigned char globe_bits[];
+
+#define GLOBE_SM_WIDTH 512
+#define GLOBE_SM_HEIGHT 512
+extern const unsigned char globe_sm_bits[];
+
+#define GLOBE_XS_WIDTH 256
+#define GLOBE_XS_HEIGHT 256
+extern const unsigned char globe_xs_bits[];
+
+#define GLOBE_XXS_WIDTH 128
+#define GLOBE_XXS_HEIGHT 128
+extern const unsigned char globe_xxs_bits[];
+
+#define MAP_WIDTH 2048
+#define MAP_HEIGHT 919
+extern const unsigned char map_bits[];
+
+#define MAP_S_WIDTH 512
+#define MAP_S_HEIGHT 230
+extern const unsigned char map_s_bits[];
+
+#define MAP_320X144_WIDTH 320
+#define MAP_320X144_HEIGHT 144
+extern const unsigned char map_320x144_bits[];
+
+#endif // __ASSETS_H__
